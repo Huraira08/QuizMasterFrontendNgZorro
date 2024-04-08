b03332c393feff4f129f13c5d6c487b503f01cb9
Muhammad Huraira <muhammadhuraira4@gmail.com>
2024-04-08 15:44:50 +0500

    added timer on quiz

--- src/app/pages/quiz-page/quiz-page.component.ts
+++ src/app/pages/quiz-page/quiz-page.component.ts
@@ -6,6 +6,7 @@ import { QuizService } from 'src/app/services/quiz.service';
 import { Result } from 'src/app/interfaces/result';
 import { NzModalService } from 'ng-zorro-antd/modal';
 import { ResultZorroModalComponent } from 'src/app/components/result-zorro-modal/result-zorro-modal.component';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-quiz-page',
@@ -17,6 +18,8 @@ export class QuizPageComponent implements OnInit {
   quizItems: Array<QuizItem> = []
   quizItemIndex!:number;
   score!: number;
+
+  timer: number = 240;
 
 
   constructor(private quizService: QuizService,
@@ -36,6 +39,17 @@ export class QuizPageComponent implements OnInit {
     
     this.quizItemIndex = 0;
     this.score = 0;
+
+    let intervalObs = interval(1000)
+    let subscription = intervalObs.subscribe({
+      next:((value)=>{
+        this.timer -= 1;
+        if(this.timer === 0) {
+          subscription.unsubscribe()
+          // this.submitResult()
+        }
+      })
+    })
   }
 
   receiveAnswer(answer:number){
@@ -49,6 +63,31 @@ export class QuizPageComponent implements OnInit {
     }
   }
 
+  submitResult(){
+    this.modalService.create({
+      nzContent: ResultZorroModalComponent,
+      nzData:{
+        score: this.score
+      },
+      nzFooter: null,
+      nzWidth: "716px",
+      nzClosable: false
+    })
+
+    this.modalService.afterAllClose.subscribe(async ()=>{
+      const newResult = {id: '',name:'', attemptedDate: new Date(Date.now()), score: this.score, }
+      this.quizService.addResult(newResult)
+      .then(response=>{
+        console.log(response);
+         this.router.navigate(['/start-quiz'], {replaceUrl:true})
+      })
+      .catch(error=>{
+         console.log(error);
+         this.router.navigate(['/start-quiz'], {replaceUrl:true})
+      })
+    })
+  }
+
   handleNextClick(){
     if(this.answerIndex < 0)return;
     if(this.quizItemIndex < this.quizItems.length){
@@ -57,28 +96,11 @@ export class QuizPageComponent implements OnInit {
       this.answerIndex = -1;
     }
     if(this.quizItemIndex >= this.quizItems.length){
-      this.modalService.create({
-        nzContent: ResultZorroModalComponent,
-        nzData:{
-          score: this.score
-        },
-        nzFooter: null,
-        nzWidth: "716px",
-        nzClosable: false
-      })
-
-      this.modalService.afterAllClose.subscribe(async ()=>{
-        const newResult = {id: '',name:'', attemptedDate: new Date(Date.now()), score: this.score, }
-        this.quizService.addResult(newResult)
-        .then(response=>{
-          console.log(response);
-           this.router.navigate(['/start-quiz'], {replaceUrl:true})
-        })
-        .catch(error=>{
-           console.log(error);
-           this.router.navigate(['/start-quiz'], {replaceUrl:true})
-        })
-      })
+      this.submitResult();
     }
+  }
+  
+  floor(value: number){
+    return Math.floor(value);
   }
 }
